a1ca8d385cea439d3c7df3ed139a7d3996ca5bd3
marianamoiolicapelari <marianamoioli@gmail.com>
2022-04-27 11:02:00 -0300

    added new exersises

---
+++ funcoes return.js
@@ -0,0 +1,11 @@
+function sum(number1, number2){
+  const result = number1 + number2
+  return result
+}
+
+const firstnumber = 10
+const secondnumber = 20
+
+console.log(`O primeiro número é ${firstnumber}`)
+console.log(`O segundo número é ${secondnumber}`)
+console.log(`E a soma dos dois é ${sum(firstnumber, secondnumber)}`) 
---
+++ funcoes.js
@@ -0,0 +1,13 @@
+function printingAboutCodeClub(){
+  console.log('Estou aprendendo muito com o CodeClub')
+}
+
+printingAboutCodeClub()
+
+
+function sum(number1, number2){
+  console.log(number1 + number2)
+}
+
+sum(1,2) // INVOCA A FUNÇÃO // INVOKE OR CALL
+sum(10,20)
